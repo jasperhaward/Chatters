b635b4b042ad69a43eda5eac45a0f0603200e130
Jasper Haward <jasperhaward@virginmedia.com>
2022-03-04 09:44:44 +0000

    Add declaration for styled components

--- src/client/declarations.d.ts
+++ src/client/declarations.d.ts
@@ -1,4 +1,10 @@
+import { JSX } from "preact"
+
 declare module "*.scss" {
     const styles: Record<string, string>;
     export default styles;
+}
+
+declare namespace React {
+    type MouseEventHandler<Target extends EventTarget> = JSX.MouseEventHandler<Target>
 }
